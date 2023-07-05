e9e1df9263ca54376ee6535185a8de4222aa8c51
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-05 15:49:30 +0200

    imp[orte vue application framework

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Dischi Json</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
-    
+    <script src="./script/script.js" type="module"></script>
+    <div id="app">
+        {{ message }}
+    </div>
 </body>
 </html>
