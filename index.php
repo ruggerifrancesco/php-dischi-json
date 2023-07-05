6aebbe8a45be392d4a1a0f897b51145e43078b5b
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-05 15:52:17 +0200

    imported boostrap and style.css

--- index.php
+++ index.php
@@ -6,12 +6,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Dischi Json</title>
+
+    <!-- Vue JS CDN -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
     <script src="./script/script.js" type="module"></script>
     <div id="app">
         {{ message }}
     </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
