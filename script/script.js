97fb4aacf61eb6fd2b073bef0dde864248442a04
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-05 16:28:42 +0200

    axios call api method and varibales

--- script/script.js
+++ script/script.js
@@ -1,9 +1,31 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  }
+    data() {
+        return {
+            message: 'Hello Vue!',
+            apiUrl: './server.php',
+            discList: []
+        }
+    },
+    methods: {
+        axiosCallApi (url) {  
+            // Optionally the request above could also be done as
+            axios.get(url, {
+                params: {
+                }
+            })
+            .then((response) => {
+              console.log(response.data);
+              this.discList = response.data
+
+            })
+            .catch(function (error) {
+              console.log(error);
+            })
+        }
+    },
+    created() {
+        this.axiosCallApi(apiUrl)
+    },
 }).mount('#app')
